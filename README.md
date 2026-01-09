f544d79637bc75ebfc91a008f5f9af0eaeb33474
jrsbm <joris.boom@xs4all.nl>
2026-01-09 15:26:23 +0000

    Improved visualisation

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # SNA-R #
 This repository contains some simple examples of the use of Social Network Analysis using R
 
+# EU Cluster for Food Traceability and Trust #
+The dataset used for this example contains the Horizon funded projects of the EU Cluster for Food Traceability and Trust and their consortiums. The social network analysis shows the interconnection of projects and consortiums.
+
+This information was filtered from a larger dataset containing all Horizon Europe projects. This dataset titled, CORDIS - EU research projects under HORIZON EUROPE (2021-2027), can be found here:
+https://data.europa.eu/data/datasets/cordis-eu-research-projects-under-horizon-europe-2021-2027?locale=en
+
+More information on the cluster can be found at:
+https://theros-project.eu/our-cluster/
--- SNA.R
+++ SNA.R
@@ -3,10 +3,10 @@ library(igraph)
 library(ggraph)
 
 # Read csv
-d <- as_tibble(read.csv('biadjacency matrix.csv'))
+d <- as_tibble(read.csv("biadjacency matrix.csv"))
 
 # Convert to matrix and assign row names
-matrix <- as.matrix(d[,2:ncol(d)])
+matrix <- as.matrix(d[, 2:ncol(d)])
 rownames(matrix) <- d[[colnames(d)[1]]]
 matrix[is.na(matrix)] <- 0
 
@@ -25,11 +25,15 @@ p <- ggraph(g_bipartite, layout = "fr") +
   geom_edge_link(edge_colour = "grey") +
   geom_node_point(aes(fill = type, size = degree(g_bipartite)),
                   shape = 21, color = "white", stroke = 0.5) +
-  geom_node_text(aes(label = name, color = type),
-                 repel = TRUE, size = 2, show.legend = FALSE) +
   scale_fill_manual(values = c("FALSE" = "#0d0dbd", "TRUE" = "#067906"),
-                    labels = c("Entity", "Group")) +
-  scale_colour_manual(values = c("FALSE" = "#3490af", "TRUE" = "#e21111")) +
+                    labels = c("Entity", "Group"), name = "Type") +
+  scale_size_continuous(range = c(2, 10), name = "Degree") +
+  geom_node_text(aes(label = name, color = "#3490af"),
+                 data = . %>% filter(type == TRUE),
+                 repel = TRUE, size = 5, show.legend = FALSE) +
+  geom_node_text(aes(label = name, color = "#e21111"),
+                 data = . %>% filter(type == FALSE),
+                 repel = TRUE, size = 2, show.legend = FALSE) +
   theme_graph() +
   labs(title = "Network Graph")
 print(p, width = 16, height = 8, units = "in", dpi = 300)
